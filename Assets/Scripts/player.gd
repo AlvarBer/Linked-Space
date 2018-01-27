0af7bfd7e9d369b1ca93145693cac6fc860aaf37
NoisyBass <laura.dcs4@gmail.com>
2018-01-27 18:13:52 +0000

    Added more sprites and players animations

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -2,24 +2,33 @@ extends Node2D
 
 export var player_idx = 1
 export var speed = 100
+export(Texture) var texture
 var holding_obj
 var last_move = Vector2(0, 0)
+onready var anim_player = $AnimationPlayer
+var anim = ""
 
 func _ready():
+	$KinematicBody2D/Sprite.set_texture(texture)
 	set_process(true)
 
 func _process(delta):
 	var movement = Vector2(0, 0)
 	var result = null
+	var new_anim = "idle"
 	
 	if Input.is_action_pressed("player_%d_left" % player_idx):
 		movement += Vector2(-1, 0)
+		new_anim = "walk_left"
 	elif Input.is_action_pressed("player_%d_right" % player_idx):
 		movement += Vector2(1, 0)
+		new_anim = "walk_right"
 	elif Input.is_action_pressed("player_%d_up" % player_idx):
 		movement += Vector2(0, -1)
+		new_anim = "walk_up"
 	elif Input.is_action_pressed("player_%d_down" % player_idx):
 		movement += Vector2(0, 1)
+		new_anim = "walk_down"
 	
 	if movement != Vector2(0, 0):
 		result = $KinematicBody2D.move_and_collide(movement * speed * delta)
@@ -41,3 +50,8 @@ func _process(delta):
 			holding_obj.get_parent().remove_child(holding_obj)
 			$KinematicBody2D.add_child(holding_obj)
 			holding_obj.position = Vector2(0, 0)
+			
+	if new_anim != anim:
+		anim = new_anim
+		anim_player.play(anim)
+		
