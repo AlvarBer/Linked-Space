3f4a09b6aec204cd30beec7d8fae75c8ee6f8c94
NoisyBass <laura.dcs4@gmail.com>
2018-01-28 13:09:55 +0000

    Added new player animations and polish to the map

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -16,7 +16,7 @@ func _ready():
 func _process(delta):
 	var movement = Vector2(0, 0)
 	var result = null
-	var new_anim = "idle"
+	var new_anim = ""
 	
 	# Check inputs
 	if Input.is_action_pressed("player_%d_left" % player_idx):
@@ -27,17 +27,28 @@ func _process(delta):
 		new_anim = "walk_right"
 	elif Input.is_action_pressed("player_%d_up" % player_idx):
 		movement += Vector2(0, -1)
-		new_anim = "walk_up"
+		new_anim = "walk_top"
 	elif Input.is_action_pressed("player_%d_down" % player_idx):
 		movement += Vector2(0, 1)
-		new_anim = "walk_down"
+		new_anim = "walk_bot"
 	
 	# Do actual movement
 	if movement != Vector2(0, 0):
 		result = $KinematicBody2D.move_and_collide(movement * speed * delta)
 		if not result:
 			last_move = movement
-
+	
+	if new_anim == "":
+		if last_move == Vector2(1, 0):
+			new_anim = "idle_right"
+		elif last_move == Vector2(-1, 0):
+			new_anim = "idle_left"
+		elif last_move == Vector2(0, 1):
+			new_anim = "idle_bot"
+		elif last_move == Vector2(0, -1):
+			new_anim = "idle_top"
+		else:
+			new_anim = "idle_right"
 	if new_anim != anim:
 		anim = new_anim
 		anim_player.play(anim)
